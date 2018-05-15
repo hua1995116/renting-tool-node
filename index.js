0fd0b130e1f4299320341e2be5e59923489a82d8
hua1995116 <461249104@qq.com>
2018-05-15 19:04:39 +0800

    U: first commit

---
+++ index.js
@@ -0,0 +1,64 @@
+var express = require('express')
+var fs = require('fs');
+var multer  = require('multer');
+var app = express()
+var router = express.Router();
+
+router.get('/', function (req, res, next) {
+    req.url = './index.html';
+    next();
+});
+app.use(router);
+
+// 允许跨域访问
+app.all('*', function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS");
+    res.header("X-Powered-By", ' 3.2.1')
+    // res.header("Content-Type", "application/json;charset=utf-8");
+    next();
+});
+
+var storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        // 接收到文件后输出的保存路径（若不存在则需要创建）
+        cb(null, 'upload/');    
+    },
+    filename: function (req, file, cb) {
+        // 将保存文件名设置为 时间戳 + 文件原始名，比如 151342376785-123.jpg
+        cb(null, Date.now() + "-" + file.originalname);  
+    }
+});
+
+// 创建文件夹
+var createFolder = function(folder){
+    try{
+        // 测试 path 指定的文件或目录的用户权限,我们用来检测文件是否存在
+        // 如果文件路径不存在将会抛出错误"no such file or directory"
+        fs.accessSync(folder); 
+    }catch(e){
+        // 文件夹不存在，以同步的方式创建文件目录。
+        fs.mkdirSync(folder);
+    }  
+};
+
+var uploadFolder = './upload/';
+createFolder(uploadFolder);
+
+// 创建 multer 对象
+var upload = multer({ storage: storage });
+
+/* POST upload listing. */
+app.post('/upload', upload.single('file'), function(req, res, next) {
+    var file = req.file;
+    console.log('文件类型：%s', file.mimetype);
+    console.log('原始文件名：%s', file.originalname);
+    console.log('文件大小：%s', file.size);
+    console.log('文件保存路径：%s', file.path);
+    // 接收文件成功后返回数据给前端
+    res.json({res_code: '0'});
+});
+
+app.use(express.static('./'));
+const server = app.listen(7788)
